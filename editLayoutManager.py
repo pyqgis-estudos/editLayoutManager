f01016ed6221ae2537f319e729c633f9127be8cd
mbaliu <38162096+mbaliu@users.noreply.github.com>
2019-03-08 16:49:46 -0300

    Update editLayoutManager.py

--- editLayoutManager.py
+++ editLayoutManager.py
@@ -3,6 +3,7 @@
 layoutS = QgsProject.instance().layoutManager().printLayouts()
 
 for layout in layoutS:
+    
     print(
     layout.name()
     layout.referenceMap().extent()
@@ -14,10 +15,15 @@ xmax = _float
 ymax = _float
 
 rect = QgsRectangle(xmin, ymin, xmax, ymax )
-layout.referenceMap().setExtent()
 
-
-
+def changeExtent(nRectangle):
+    layout.referenceMap().setExtent()
+    
+# Find the items fromthe layout
+def items():
+    layout.items()
+     
+             
 '''
 REF
 https://gis.stackexchange.com/questions/67084/setting-the-extent-for-a-qgis-map-using-python
